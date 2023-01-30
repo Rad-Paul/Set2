6c6dce161282163bcd7d42e14a64f9c32bf4fb23
Rad-Paul <115529048+Rad-Paul@users.noreply.github.com>
2023-01-30 05:51:33 +0200

    Add files via upload

---
+++ set2ex13/set2ex13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace set2ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O <secventa crescatoare rotita> este o secventa de numere care este in ordine crescatoare
+            //sau poate fi transformata intr-o secventa in ordine crescatoare prin rotiri succesive
+            //(rotire cu o pozitie spre stanga = toate elementele se muta cu o pozitie spre stanga si primul element devine ultimul).
+            //Determinati daca o secventa de n numere este o secventa crescatoare rotita. 
+
+        }
+    }
+}
